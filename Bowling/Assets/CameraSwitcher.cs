3e24e2f9664c677cdeff5855020f19b7c044b0c3
Denis Lewandowski <denis.lewandowski@aptitudesoftware.com>
2019-12-03 00:23:17 +0100

    Second Pin Camera added + LCtrl switching

---
+++ Bowling/Assets/CameraSwitcher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera freeCam;
+    public Camera pinCam;
+
+    private int clickCounter = 0;
+    private bool duringSwitching = true;
+
+    public void SwitchCamera(int x)
+    {
+        if (x == 0)
+        {
+            freeCam.enabled = true;
+        }
+        else if (x == 1)
+        {
+            pinCam.enabled = true;
+        }
+    }
+
+    public void DisableAll()
+    {
+        freeCam.enabled = false;
+        pinCam.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !duringSwitching)
+        {
+            duringSwitching = true;
+            clickCounter++;
+            DisableAll();
+            SwitchCamera(clickCounter % 2);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            duringSwitching = false;
+        }
+    }
+}
